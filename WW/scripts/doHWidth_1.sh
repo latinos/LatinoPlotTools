f4c5ee611fb853d8e6d33308794479f2f84d9b7d
amassiro <massironi.andrea@gmail.com>
2014-11-25 15:44:28 +0100

    update hwidth

--- WW/scripts/doHWidth_1.sh
+++ WW/scripts/doHWidth_1.sh
@@ -1,7 +1,11 @@
 # 
 # 
 # 
-root -l finalPlot.C+\(0,1,\"m_{ll}:m_{T}^{H}/mva\",\"\",\"data/hww_mll_0j.root\",\"outfile\",0,125,19.4,0,1,18\)
+root -l -q finalPlot.C+\(11,1,\"mll\ vs\ m_{T}^{H}\/mva\",\"\",\"/home/amassiro/Cern/Code/VBF/qqHWW/AnalysisPackage_qqHWWlnulnu/test/HWidth/Plot/final_6.root\",\"mllmthmva_1jet\",0,125,19.4,0,1,0,20,10,800,600\)
+root -l -q  finalPlot.C+\(11,1,\"mll\ vs\ m_{T}^{H}\/mva\",\"\",\"/home/amassiro/Cern/Code/VBF/qqHWW/AnalysisPackage_qqHWWlnulnu/test/HWidth/Plot/final_4.root\",\"mllmthmva_0jet\",0,125,19.4,0,1,0,41,22,800,600\)
+root -l -q finalPlot.C+\(11,1,\"mll\ vs\ m_{T}^{H}\/mva\",\"\",\"/home/amassiro/Cern/Code/VBF/qqHWW/AnalysisPackage_qqHWWlnulnu/test/HWidth/Plot/final_0.root\",\"mllmth_0jet\",0,125,19.4,0,1,0,33,22,800,600\)
+root -l -q  finalPlot.C+\(11,1,\"mll\ vs\ m_{T}^{H}\/mva\",\"\",\"/home/amassiro/Cern/Code/VBF/qqHWW/AnalysisPackage_qqHWWlnulnu/test/HWidth/Plot/final_2.root\",\"mllmth_1jet\",0,125,19.4,0,1,0,26,26,800,600\)
+root -l -q  finalPlot.C+\(11,1,\"mll\ vs\ m_{T}^{H}\/mva\",\"\",\"/home/amassiro/Cern/Code/VBF/qqHWW/AnalysisPackage_qqHWWlnulnu/test/HWidth/Plot/final_3.root\",\"mllmth_2jet\",0,125,19.4,0,1,0,3,5,500,500\)
 # 
 # 
 # finalPlot (int nsel             = 0, 
@@ -15,6 +19,11 @@ root -l finalPlot.C+\(0,1,\"m_{ll}:m_{T}^{H}/mva\",\"\",\"data/hww_mll_0j.root\"
 #            double lumi          = 4.6, 
 #            bool doDataMCRatio   = true, 
 #            int signalZoom       = 1,
-#            int labelType        = 0
+#            int labelType        = 0,
+#            int blindSX          = 0,
+#            int blindDX          = 0,
+#            int XCanvas          = 500,
+#            int YCanvas          = 500
+#           )
 # 
 # 
